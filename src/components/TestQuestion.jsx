bed064fef90c4b72e25c6f0c5c19d2ed8b1a0548
BSenichak <bsenk77@gmail.com>
2024-11-05 16:16:08 +0200

    add bootstrap

--- src/components/TestQuestion.jsx
+++ src/components/TestQuestion.jsx
@@ -5,22 +5,23 @@ export class TestQuestion extends Component {
 
     render() {
         return (
-        <div className={styles.question}>
-            <h2>Question #{this.props.questionId}: {this.props.questionText}</h2>
-            <ul>
+        <div className={`${styles.question} card`}>
+            <h3 className="card-header">Question #{this.props.questionId}: {this.props.questionText}</h3>
+            <ul className="card-body">
                 {this.props.answers.map((answer, index) => (
-                    <li key={index}>
+                    <li key={index} className="form-check">
                         <input
                             type="radio"
                             id={`answer${index + 1}`}
                             name="answer"
                             value={answer}
+                            className="form-check-input"
                         />
-                        <label htmlFor={`answer${index + 1}`}>{answer}</label>
+                        <label htmlFor={`answer${index + 1}`} className="form-check-label">{answer}</label>
                     </li>
                 ))}                
             </ul>
-            <button>Submit</button>
+            <button className="btn btn-primary m-2">Submit</button>
         </div>
         );
     }
