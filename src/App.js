b8895bbc3fbe09f28467c0c20675a80b2a7976ea
BSenichak <bsenk77@gmail.com>
2024-11-06 12:07:58 +0200

    add swithing between questions

--- src/App.js
+++ src/App.js
@@ -3,18 +3,154 @@ import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 import TestQuestion from "./components/TestQuestion.jsx";
 import styles from "./styles/components/app.module.scss";
+import StartQuiz from "./components/StartQuiz.jsx";
 
 export class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isQuizStarted: false,
+            questionNumber: 0,
+        };
+        this.startQuiz = this.startQuiz.bind(this);
+        this.nextQuestion = this.nextQuestion.bind(this);
+        this.previousQuestion = this.previousQuestion.bind(this);
+        this.stopQuiz = this.stopQuiz.bind(this);
+    }
+
+    quiz1 = {
+        quizName: "HTML test",
+        quizDescription: "Test your HTML knowledge",
+        questions: [
+            {
+                questionId: 1,
+                questionText: "Which tag is used for styling?",
+                answers: ["h1", "style", "script", "input"],
+                correctAnswer: "style",
+            },
+            {
+                questionId: 2,
+                questionText:
+                    "What is the correct HTML tag for inserting a line break?",
+                answers: ["br", "break", "lb", "line"],
+                correctAnswer: "br",
+            },
+            {
+                questionId: 3,
+                questionText: "Which tag is used to define a hyperlink?",
+                answers: ["link", "a", "href", "hyper"],
+                correctAnswer: "a",
+            },
+            {
+                questionId: 4,
+                questionText:
+                    "Which attribute is used to specify the URL in an anchor tag?",
+                answers: ["href", "src", "link", "url"],
+                correctAnswer: "href",
+            },
+            {
+                questionId: 5,
+                questionText:
+                    "What is the correct HTML element for the largest heading?",
+                answers: ["heading", "h6", "h1", "head"],
+                correctAnswer: "h1",
+            },
+            {
+                questionId: 6,
+                questionText:
+                    "Which HTML attribute specifies an alternative text for an image?",
+                answers: ["src", "alt", "caption", "title"],
+                correctAnswer: "alt",
+            },
+            {
+                questionId: 7,
+                questionText:
+                    "Which of the following is the correct HTML for creating a checkbox?",
+                answers: [
+                    "<input type='checkbox'>",
+                    "<checkbox>",
+                    "<input checkbox='true'>",
+                    "<check>",
+                ],
+                correctAnswer: "<input type='checkbox'>",
+            },
+            {
+                questionId: 8,
+                questionText: "How can you open a link in a new tab?",
+                answers: [
+                    'target="_new"',
+                    'target="newtab"',
+                    'target="_self"',
+                    'target="_blank"',
+                ],
+                correctAnswer: 'target="_blank"',
+            },
+            {
+                questionId: 9,
+                questionText: "Which tag is used to define a table header?",
+                answers: ["th", "tr", "td", "table"],
+                correctAnswer: "th",
+            },
+            {
+                questionId: 10,
+                questionText:
+                    "Which element is used to define an unordered list?",
+                answers: ["ul", "ol", "li", "list"],
+                correctAnswer: "ul",
+            },
+        ],
+    };
+
+    startQuiz() {
+        this.setState({
+            isQuizStarted: true,
+        });
+    }
+
+    stopQuiz() {
+        this.setState({
+            isQuizStarted: false,
+        });
+    }
+
+    nextQuestion() {
+        if (this.state.questionNumber !== this.quiz1.questions.length - 1) {
+            this.setState({
+                questionNumber: this.state.questionNumber + 1,
+            });
+        }
+    }
+
+    previousQuestion() {
+        if (this.state.questionNumber > 0) {
+            this.setState({
+                questionNumber: this.state.questionNumber - 1,
+            });
+        }
+    }
+
     render() {
         return (
             <>
                 <Header />
                 <main className={styles.main}>
-                    <TestQuestion
-                        questionId={1}
-                        questionText="which tag is used for styling"
-                        answers={["h1", "style", "script", "input"]}
-                    />
+                    {this.state.isQuizStarted ? (
+                        <TestQuestion
+                            questionId={this.quiz1.questions[this.state.questionNumber].questionId}
+                            questionText={this.quiz1.questions[this.state.questionNumber].questionText}
+                            answers={this.quiz1.questions[this.state.questionNumber].answers}
+                            correctAnswer={this.quiz1.questions[this.state.questionNumber].correctAnswer}
+                            nextQuestion={this.nextQuestion}
+                            previousQuestion={this.previousQuestion}
+                            stopQuiz={this.stopQuiz}
+                        />
+                    ) : (
+                        <StartQuiz
+                            name={this.quiz1.quizName}
+                            description={this.quiz1.quizDescription}
+                            startQuiz={this.startQuiz}
+                        />
+                    )}
                 </main>
                 <Footer />
             </>
--- src/components/TestQuestion.jsx
+++ src/components/TestQuestion.jsx
@@ -2,27 +2,53 @@ import React, { Component } from "react";
 import styles from "../styles/components/question.module.scss";
 
 export class TestQuestion extends Component {
-
     render() {
         return (
-        <div className={`${styles.question} card`}>
-            <h3 className="card-header">Question #{this.props.questionId}: {this.props.questionText}</h3>
-            <ul className="card-body">
-                {this.props.answers.map((answer, index) => (
-                    <li key={index} className="form-check">
-                        <input
-                            type="radio"
-                            id={`answer${index + 1}`}
-                            name="answer"
-                            value={answer}
-                            className="form-check-input"
-                        />
-                        <label htmlFor={`answer${index + 1}`} className="form-check-label">{answer}</label>
-                    </li>
-                ))}                
-            </ul>
-            <button className="btn btn-primary m-2">Submit</button>
-        </div>
+            <div className={`${styles.question} card`}>
+                <h3 className="card-header">
+                    Question #{this.props.questionId}: {this.props.questionText}
+                </h3>
+                <ul className="card-body">
+                    {this.props.answers.map((answer, index) => (
+                        <li key={index} className="form-check">
+                            <input
+                                type="radio"
+                                id={`answer${index + 1}`}
+                                name="answer"
+                                value={answer}
+                                className="form-check-input"
+                            />
+                            <label
+                                htmlFor={`answer${index + 1}`}
+                                className="form-check-label"
+                            >
+                                {answer}
+                            </label>
+                        </li>
+                    ))}
+                </ul>
+                <button className="btn btn-primary m-2">Submit</button>
+                <div className={styles.buttons}>
+                    <button
+                        className="btn btn-primary"
+                        onClick={() => this.props.previousQuestion()}
+                    >
+                        Previous
+                    </button>
+                    <button
+                        className="btn btn-primary"
+                        onClick={() => this.props.nextQuestion()}
+                    >
+                        Next
+                    </button>
+                </div>
+                <button
+                    className={"btn btn-secondary m-2 " + styles.stop}
+                    onClick={() => this.props.stopQuiz()}
+                >
+                    Stop Quiz
+                </button>
+            </div>
         );
     }
 }
